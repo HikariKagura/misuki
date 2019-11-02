4fbb3e4fa47ca7ddd965de8e05ea38d19edcbb8c
LainNetWork <tianshang360@gmail.com>
2019-11-02 23:01:02 +0800

    添加部分注释，提交代码

--- src/main/java/fun/lain/mizuki/misuki/moudels/RouterController.java
+++ src/main/java/fun/lain/mizuki/misuki/moudels/RouterController.java
@@ -5,9 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RouterController {
-    @RequestMapping("/loginpage")
+    @RequestMapping("/login")
     public String toLogin(){
-        return "login/login";
+        //返回模路径，由于模板引擎默认配置了模板路径，以及模板后缀，
+        //因此实际上模板引擎去找的就是 classpath:/templates/user/login.html
+        return "user/login";
     }
 
+
 }
+
--- src/main/java/fun/lain/mizuki/misuki/moudels/user/controller/UserController.java
+++ src/main/java/fun/lain/mizuki/misuki/moudels/user/controller/UserController.java
@@ -1,19 +1,23 @@
 package fun.lain.mizuki.misuki.moudels.user.controller;
 
 import fun.lain.mizuki.misuki.moudels.user.service.UserService;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
-@RestController
+@Controller
+@RequestMapping("/api")
 public class UserController {
+
+    @Resource
+    UserService userService;
 
     @RequestMapping("/login")
     public String login(String email,String password){
         return "" + userService.login(email, password);
     }
 
-    @Resource
-    UserService userService;
+
 }
